7af57469ffed25b8c840ce3422414d862c119ce8
desamatheus <120420071+desamatheus@users.noreply.github.com>
2023-12-14 21:10:34 -0300

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,56 @@
+/** @format */
+
+const data = {
+  products: [
+    {
+      name: 'Nike Slim  shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: 'images/p1.jpg', // 679px x 829px
+      price: 120,
+      contInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Adidas fit shirt',
+      slug: 'adidas-fit-shirt',
+      category: 'Shirts',
+      image: 'images/p2.jpg', // 679px x 829px
+      price: 250,
+      contInStock: 20,
+      brand: 'Adidas',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Nike Slim pant',
+      slug: 'nike-slim-pant',
+      category: 'Pants',
+      image: 'images/p3.jpg',
+      price: 25,
+      contInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+    {
+      name: 'Adidadas fit pant',
+      slug: 'adidas-fit-pant',
+      category: 'Pants',
+      image: 'images/p4.jpg',
+      price: 65,
+      contInStock: 5,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+  ],
+};
+
+export default data;
